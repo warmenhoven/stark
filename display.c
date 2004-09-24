8157dac7b6056f84066a792ece4a06e8ac751679
Eric Warmenhoven <eric@warmenhoven.org>
2004-09-24 20:20:43 +0000

    just moving things

--- display.c
+++ display.c
@@ -14,118 +14,6 @@ static account *curr_acct = NULL;
 
 static list *disp_acct = NULL;
 
-static account *
-select_next_acct(account *a)
-{
-	account *p;
-	list *l;
-
-	/* first try and select a child */
-	if (a->expanded && a->subs)
-		return a->subs->data;
-
-	/* if we can't then try and select our sibling. start by thinking that maybe
-	 * we're a top-level account */
-	if (!a->parent) {
-		l = list_find(accounts, a);
-		if (l->next) {
-			return l->next->data;
-		} else {
-			/* we are a top-level account but we don't have a subsequent
-			 * sibling. also we don't have any more selectable children. so
-			 * we're done! */
-			return a;
-		}
-	}
-
-	/* we're not a top-level account, try and select our next sibling */
-	l = list_find(a->parent->subs, a);
-	if (l->next) {
-		return l->next->data;
-	}
-
-	/* we have no next sibling. try and get our parent's next sibling, etc. */
-	p = a->parent;
-	while (p) {
-		/* our parent is top-level */
-		if (!p->parent) {
-			l = list_find(accounts, p);
-			if (l->next) {
-				/* return the next top-level account */
-				return l->next->data;
-			} else {
-				/* we got to the end of the list! break. */
-				break;
-			}
-		}
-
-		l = list_find(p->parent->subs, p);
-		if (l->next) {
-			/* our parent has a sibling, let's move to them */
-			return l->next->data;
-		} else {
-			/* our parent is the last child, move to our parent's parent */
-			p = p->parent;
-		}
-	}
-
-	/* we are the end of the line. return ourselves! */
-	return a;
-}
-
-static account *
-get_last_expanded(account *a)
-{
-	while (a->expanded && a->subs) {
-		list *l = a->subs;
-		while (l->next) l = l->next;
-		a = l->data;
-	}
-
-	return a;
-}
-
-static account *
-select_prev_acct(account *a)
-{
-	list *l;
-
-	if (!a->parent) {
-		list *l = list_find(accounts, a);
-		if (l->prev) {
-			return get_last_expanded(l->prev->data);
-		} else {
-			return a;
-		}
-	}
-
-	l = list_find(a->parent->subs, a);
-	if (l->prev) {
-		return get_last_expanded(l->prev->data);
-	} else {
-		return a->parent;
-	}
-}
-
-static float
-get_value(account *a)
-{
-	float total = 0;
-	list *l = a->subs;
-
-	if (a->commodity && a->commodity->quote)
-			total = a->quantity * a->commodity->quote->value;
-	else if (!a->commodity)
-		total += a->quantity;
-
-	while (l) {
-		total += get_value(l->data);
-		l = l->next;
-	}
-
-	return total;
-}
-
 static int
 build_exp_accts(list *accts, list **l)
 {
@@ -145,31 +33,23 @@ build_exp_accts(list *accts, list **l)
 	return i;
 }
 
-static void
-recalc_skip()
+static float
+get_value(account *a)
 {
-	list *exp = NULL;
-	list *l;
-	int len;
-	int i;
+	float total = 0;
+	list *l = a->subs;
 
-	len = build_exp_accts(accounts, &exp);
+	if (a->commodity && a->commodity->quote)
+			total = a->quantity * a->commodity->quote->value;
+	else if (!a->commodity)
+		total += a->quantity;
 
-	l = list_find(exp, curr_acct);
-	i = list_length(l->next);
-
-	list_free(exp);
-
-	if ((len - skip) - i < LINES) {
-		/* len - skip is how many accounts are displayed from the first one
-		 * that's not skipped. (len - skip) - i is how many accounts there are
-		 * between the first displayed and the current account. if that value is
-		 * less than the number of lines, then the current account is still
-		 * displayed and we don't need to adjust the skip. */
-		return;
+	while (l) {
+		total += get_value(l->data);
+		l = l->next;
 	}
 
-	skip = len - LINES - i;
+	return total;
 }
 
 static void
@@ -369,14 +249,6 @@ redraw_screen()
 }
 
 static void
-display_end()
-{
-	clear();
-	refresh();
-	endwin();
-}
-
-static void
 expand_all(account *a)
 {
 	list *l = a->subs;
@@ -406,6 +278,126 @@ close_all(account *a)
 		close_all(l->data);
 		l = l->next;
 	}
+}
+
+static account *
+select_next_acct(account *a)
+{
+	account *p;
+	list *l;
+
+	/* first try and select a child */
+	if (a->expanded && a->subs)
+		return a->subs->data;
+
+	/* if we can't then try and select our sibling. start by thinking that maybe
+	 * we're a top-level account */
+	if (!a->parent) {
+		l = list_find(accounts, a);
+		if (l->next) {
+			return l->next->data;
+		} else {
+			/* we are a top-level account but we don't have a subsequent
+			 * sibling. also we don't have any more selectable children. so
+			 * we're done! */
+			return a;
+		}
+	}
+
+	/* we're not a top-level account, try and select our next sibling */
+	l = list_find(a->parent->subs, a);
+	if (l->next) {
+		return l->next->data;
+	}
+
+	/* we have no next sibling. try and get our parent's next sibling, etc. */
+	p = a->parent;
+	while (p) {
+		/* our parent is top-level */
+		if (!p->parent) {
+			l = list_find(accounts, p);
+			if (l->next) {
+				/* return the next top-level account */
+				return l->next->data;
+			} else {
+				/* we got to the end of the list! break. */
+				break;
+			}
+		}
+
+		l = list_find(p->parent->subs, p);
+		if (l->next) {
+			/* our parent has a sibling, let's move to them */
+			return l->next->data;
+		} else {
+			/* our parent is the last child, move to our parent's parent */
+			p = p->parent;
+		}
+	}
+
+	/* we are the end of the line. return ourselves! */
+	return a;
+}
+
+static account *
+get_last_expanded(account *a)
+{
+	while (a->expanded && a->subs) {
+		list *l = a->subs;
+		while (l->next) l = l->next;
+		a = l->data;
+	}
+
+	return a;
+}
+
+static account *
+select_prev_acct(account *a)
+{
+	list *l;
+
+	if (!a->parent) {
+		list *l = list_find(accounts, a);
+		if (l->prev) {
+			return get_last_expanded(l->prev->data);
+		} else {
+			return a;
+		}
+	}
+
+	l = list_find(a->parent->subs, a);
+	if (l->prev) {
+		return get_last_expanded(l->prev->data);
+	} else {
+		return a->parent;
+	}
+}
+
+static void
+recalc_skip()
+{
+	list *exp = NULL;
+	list *l;
+	int len;
+	int i;
+
+	len = build_exp_accts(accounts, &exp);
+
+	l = list_find(exp, curr_acct);
+	i = list_length(l->next);
+
+	list_free(exp);
+
+	if ((len - skip) - i < LINES) {
+		/* len - skip is how many accounts are displayed from the first one
+		 * that's not skipped. (len - skip) - i is how many accounts there are
+		 * between the first displayed and the current account. if that value is
+		 * less than the number of lines, then the current account is still
+		 * displayed and we don't need to adjust the skip. */
+		return;
+	}
+
+	skip = len - LINES - i;
 }
 
 static int
@@ -490,7 +482,6 @@ list_handle_key(int c)
 		}
 		break;
 	case 'q':
-		display_end();
 		return 1;
 	case '-':
 		if (curr_acct->subs) {
@@ -638,12 +629,20 @@ display_run()
 	while ((c = getch())) {
 		switch (display_mode) {
 		case ACCT_LIST:
-			if (list_handle_key(c))
+			if (list_handle_key(c)) {
+				clear();
+				refresh();
+				endwin();
 				return;
+			}
 			break;
 		case ACCT_DETAIL:
-			if (detail_handle_key(c))
+			if (detail_handle_key(c)) {
+				clear();
+				refresh();
+				endwin();
 				return;
+			}
 			break;
 		}
 	}
