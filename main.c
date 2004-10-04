9f2495676e76f8118441c63205df01963b6b1060
Eric Warmenhoven <eric@warmenhoven.org>
2004-10-04 17:35:11 +0000

    succeeded in 0.218055 sec

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
+#include <unistd.h>
 #include "main.h"
 
 static void
@@ -12,13 +15,24 @@ usage(char *name)
 int
 main(int argc, char **argv)
 {
+	struct timeval begin, end;
+
 	if (argc != 2)
 		usage(argv[0]);
 
+	printf("opening %s...\n", argv[1]);
+	gettimeofday(&begin, NULL);
 	gnucash_init(argv[1]);
+	gettimeofday(&end, NULL);
+	printf("succeeded in %lu.%06lu sec\n",
+		   (end.tv_sec - begin.tv_sec),
+		   (end.tv_usec - begin.tv_usec));
 
-	if (!getenv("STARK_TIME"))
-		display_run();
+	display_run();
+
+#ifdef DEBUG
+	write_file("testout");
+#endif
 
 	return 0;
 }
