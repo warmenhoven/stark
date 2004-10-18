fbdb61ea3b62038fe0437bfa7407264011f97304
Eric Warmenhoven <eric@warmenhoven.org>
2004-10-18 18:16:58 +0000

    eh. we can do this with make PERF=1 now.

--- main.c
+++ main.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
-#include <time.h>
-#include <unistd.h>
 #include "main.h"
 
 static void
@@ -15,18 +12,10 @@ usage(char *name)
 int
 main(int argc, char **argv)
 {
-	struct timeval begin, end;
-
 	if (argc != 2)
 		usage(argv[0]);
 
-	printf("opening %s...\n", argv[1]);
-	gettimeofday(&begin, NULL);
 	gnucash_init(argv[1]);
-	gettimeofday(&end, NULL);
-	printf("succeeded in %lu.%06lu sec\n",
-		   (end.tv_sec - begin.tv_sec),
-		   (end.tv_usec - begin.tv_usec));
 
 	display_run(argv[1]);
 
